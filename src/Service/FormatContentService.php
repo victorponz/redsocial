b39959ba77af0e77ce33cc777018714c96194273
victorponz <victorponz@gmail.com>
2024-01-18 12:32:43 +0100

    Usar urlGenerator para generar las urls

--- src/Service/FormatContentService.php
+++ src/Service/FormatContentService.php
@@ -19,16 +19,25 @@ class FormatContentService
         El segundo, (.+), coincide con cualquier texto entre paréntesis.
         */
         $expression = '/(https?:\/\/\S+)/';
-        return preg_replace($expression, '<a href="\1">\1</a>', $content);
+        //return preg_replace($expression, '<a href="\1">\1</a>', $content);
+        return \preg_replace_callback($expression, 
+            function ($matches) {
+                    return "<a href='".  $matches[0] . "'>" . $matches[0] . "</a>";               
+            },
+            $content);
 
     }
     private function replaceMention(string $content): string
     {
         /* Capturar el grupo delimitado por el carácter @ y fin de línea o espacio
         */
-        $expression = "/@([a-zA-Z0-9_-]+)/";
+        $expression = "/@[a-zA-Z0-9_-]+/";
         // Replace @mention with the HTML code using regular expression
-        return preg_replace($expression, '<a href="/tweets/user/@\1">@\1</a>', $content);
+        return \preg_replace_callback($expression, 
+            function ($matches) {
+                return "<a href='" . $this->urlGenerator->generate("user_tweets", ['username'=>substr($matches[0], 1)]) . "'>" . $matches[0] . "</a>";
+            },
+            $content);
 
     }
     private function replaceHashtag(string $content): string
@@ -37,7 +46,11 @@ class FormatContentService
         */
         $expression = "/#([a-zA-Z0-9_-]+)/";
         // Replace @mention with the HTML code using regular expression
-        return preg_replace($expression, '<a href="/tweets/hashtag/\1">#\1</a>', $content);
+        return \preg_replace_callback($expression, 
+            function ($matches) {
+                return "<a href='" . $this->urlGenerator->generate("hashtag_tweets", ['hashtag'=>substr($matches[0], 1)]) . "'>" . $matches[0] . "</a>";
+            },
+            $content);
     }
 
     public function format(string $originalContent): string
